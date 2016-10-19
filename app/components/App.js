42f350f4eb12df6fbcd0d030faeb68da324004c5
Matt Catellier <mattcatellier@gmail.com>
2016-10-19 13:26:49 -0700

    Checkin broken code

--- app/components/App.js
+++ app/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Footer from './Footer';
+import NavBar from './NavBar';
 
 class App extends React.Component {
     render() {
         return (
             <div>
+                <NavBar history='this.props.history'></NavBar>
                 {this.props.children}
+                <Footer />
             </div>
         );
     }
